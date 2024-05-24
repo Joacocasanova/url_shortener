da95eb053bbdec62de7cc8a63890a9166bf68c9c
Joaco Casanova <joaco.c91@gmail.com>
2024-05-24 15:15:02 -0300

    add views and url counter functions

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,14 @@
 class Link < ApplicationRecord
+    has_many :views, dependent: :destroy
     scope :recent_first, -> { order(created_at: :desc) }
 
     validates :url, presence: true
+
+    def to_param
+        ShortCode.encode(id)
+    end
+
+    def self.find(id)
+        super ShortCode.decode(id)
+    end
 end
